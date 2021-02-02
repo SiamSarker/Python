00c884d244482fe76bb6f26962d04cf3c2681458
SiamSarker <sarkersiam2@gmail.com>
2021-02-03 00:46:59 +0600

    hi

--- Udemy/Basics/Password.py
+++ Udemy/Basics/Password.py
@@ -4,6 +4,8 @@ password = input('What is your password?')
 password_length = len(password)
 hidden_password = '*' * password_length
 
+
+
 print(
     f'{username}, your password, {hidden_password},  is {password_length} letters long'
 )
