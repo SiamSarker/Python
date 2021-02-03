59e0d55b105dd90695e7d7b124adacd7950f571e
SiamSarker <sarkersiam2@gmail.com>
2021-02-03 19:54:31 +0600

    List

---
+++ Udemy/Basics/List.py
@@ -0,0 +1,33 @@
+li = [1,2,3, 4, 5]
+li2 = ['a', 'b', 'c']
+li3 = [1,2,'a', True]
+
+# list is from of array
+
+# Data Structure
+print(li2[2])
+
+# List slicing
+
+amazon_cart = [
+    'notebooks',
+    'sunglasses',
+    'toys',
+    'grapes'
+]
+
+# lits are mutable
+
+amazon_cart[0] = 'laptop'
+
+print(amazon_cart)
+
+print(amazon_cart[1:3])
+
+new_cart = amazon_cart[:]
+
+# if you wanna copy a list use slicing lake  new_cart = amazon_cart[:]
+# else it's just use as pointers
+
+
+
