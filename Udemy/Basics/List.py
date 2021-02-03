4102726a9386c34f88bf43e1fda67a35a7d3a178
SiamSarker <sarkersiam2@gmail.com>
2021-02-03 20:36:09 +0600

    List

--- Udemy/Basics/List.py
+++ Udemy/Basics/List.py
@@ -30,4 +30,85 @@ new_cart = amazon_cart[:]
 # else it's just use as pointers
 
 
+basket = [1,2,3,4,5]
+print(len(basket))
+
+# adding
+basket.append(100)
+new_list = basket
+print(new_list)
+print(basket)
+
+# insert
+basket.insert(4, 56)
+
+basket.extend([100, 1001])
+print(basket)
+
+# remove
+
+basket.pop()
+print(basket)
+
+basket.remove(4)
+print(basket)
+
+
+
+
+# index
+
+print(basket.index(100, 0, 6))
+
+
+# python keyword
+
+
+print(100 in basket)
+print('i' in 'hi')
+
+print(basket.count(100))
+
+
+basket = ['a', 'b','c', 'd', 'e', 'd']
+
+# basket.sort()
+
+print(sorted(basket))
+print(basket)
+
+
+
+basket.reverse()
+print(basket)
+
+print(list(range(100)))
+
+new_sentence = ' '.join(['hi', 'my', 'name', 'is', 'Siam'])
+
+print(new_sentence)
+
+# list unpacking
+
+a, b, c, *other, d = [1, 2, 3, 4, 5, 6]
+
+print(other)
+print(d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
