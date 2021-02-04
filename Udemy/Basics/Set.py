9e4530ac6a1952d3dcd9b837480cbbe9da448c78
SiamSarker <sarkersiam2@gmail.com>
2021-02-04 21:59:01 +0600

    Set and list

--- Udemy/Basics/Set.py
+++ Udemy/Basics/Set.py
@@ -6,3 +6,17 @@ my_list = [1,2,3,4,5,6]
 
 print(set(my_list))
 
+print('*', end='')
+print('')
+
+
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+
+duplicates = []
+
+for value in some_list:
+    if some_list.count(value) > 1:
+        if value not in duplicates:
+            duplicates.append(value)
+
+print(duplicates)
