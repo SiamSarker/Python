1ef8ff6459296e940ff3b3b1c245042d5055f8f7
SiamSarker <sarkersiam2@gmail.com>
2021-02-05 11:03:08 +0600

    oop class basics

--- Udemy/OOP/Basic.py
+++ Udemy/OOP/Basic.py
@@ -1,15 +1,30 @@
 class PlayerCharacter:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    # class Object Attribute
+    membership = True
 
-    def run(self):
-        print('run')
+    def __init__(self, name, age):
+        if self.membership:
+            self.name = name
+            self.age = age
+
+    def shout(self):
+        print(f'my name is {self.name}')
         return 'done'
+
+    @classmethod
+    def adding_things(cls, num1, num2):
+        return cls('Anas', num1 + num2)
+
+    @staticmethod
+    def adding_things2(num1, num2):
+        return num1 + num2
 
 
 player1 = PlayerCharacter('Siam', 20)
 player2 = PlayerCharacter('Tom', 18)
 
-print(player1.run())
-print(player2.name)
+print(player1.shout())
+print(player1.adding_things2(5, 6))
+
+player3 = PlayerCharacter.adding_things(8, 9)
+print(player3.age)
