9b3a7966c911bc6a3a76201d0734139ae138392b
SiamSarker <sarkersiam2@gmail.com>
2021-02-09 01:56:22 +0600

    error handling

---
+++ Udemy/Error Handling/ErrorHandle.py
@@ -0,0 +1,12 @@
+# Error handling
+
+def sum(num1, num2):
+    try:
+        return num1 / num2
+    # except TypeError as err:
+    #     print(f'please enter numbers {err}')
+    except(TypeError, ZeroDivisionError) as err:
+        print(err)
+
+
+print(sum(1, 0))
