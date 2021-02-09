871fdce3194f99117947f88bc5e5f4ddcc4b35f6
SiamSarker <sarkersiam2@gmail.com>
2021-02-09 16:31:09 +0600

    uses

---
+++ Udemy/Generators/Ugaes.py
@@ -0,0 +1,29 @@
+# Decorator
+from time import time
+def performance(fn):
+    def wrapper(*args, **kwargs):
+        t1 = time()
+        result = fn(*args, **kwargs)
+        t2 = time()
+        print(f'took {t2 - t1} ms')
+        return result
+    return wrapper
+
+
+# to look my performance
+@performance
+def long_time():
+    print('1')
+    for i in range(100000000):
+        i*5
+
+
+@performance
+def long_time2():
+    print('2')
+    for i in list(range(100000000)):
+        i*5
+
+
+long_time()
+long_time2()
