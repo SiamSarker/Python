0f9067be87c584180636daeb06ef19a6534dff92
SiamSarker <sarkersiam2@gmail.com>
2021-02-11 23:48:47 +0600

    regular expression

---
+++ Udemy/Regular Expression/EmailCheck.py
@@ -0,0 +1,28 @@
+import re
+
+pattern = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+
+string = 'uiu@ac.bd'
+
+print('search' in string)
+
+a = pattern.search(string)
+# a = re.search('this', string)
+#print(re.search('this', string))
+
+print(a.span())
+print(a.start())
+print(a.end())
+print(a.group())
+
+
+b = pattern.findall(string)
+
+print(b)
+
+c = pattern.fullmatch(string)
+print(c)
+
+d = pattern.match(string)  # first match
+print(d)
+
