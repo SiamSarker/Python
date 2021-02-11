1da62ca84426449a99557c3a21660afd02681f76
SiamSarker <sarkersiam2@gmail.com>
2021-02-11 23:13:11 +0600

    regular expression

---
+++ Udemy/Regular Expression/Basic.py
@@ -0,0 +1,27 @@
+import re
+
+pattern = re.compile('this')
+
+string = 'search this index of this text please!'
+
+print('search' in string)
+
+a = pattern.search(string)
+# a = re.search('this', string)
+#print(re.search('this', string))
+
+print(a.span())
+print(a.start())
+print(a.end())
+print(a.group())
+
+
+b = pattern.findall(string)
+
+print(b)
+
+c = pattern.fullmatch(string)
+print(c)
+
+d = pattern.match(string)  # first match
+print(d)
