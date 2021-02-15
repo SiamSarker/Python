20b7059f6c2cafc00a99da56a8a04b973ac800de
SiamSarker <sarkersiam2@gmail.com>
2021-02-15 23:50:14 +0600

    4. scripting images

--- Udemy/Scripting/Images/images.py
+++ Udemy/Scripting/Images/images.py
@@ -1,5 +1,19 @@
-from PIL import Image
+from PIL import Image, ImageFilter
 
 img = Image.open('./pikachu.jpg')
+# filtered_img = img.filter(ImageFilter.BLUR)
+# filtered_img = img.filter(ImageFilter.SMOOTH)
+filtered_img = img.convert('L')
 
-print(img)
+
+
+# filtered_img.save("blur.png", 'png')
+# filtered_img.save("smooth.png", 'png')
+filtered_img.save("grey.png", 'png')
+
+
+# print(img.size)
+# print(img.format)
+# print(img.mode)
+# print(dir(img))
+
