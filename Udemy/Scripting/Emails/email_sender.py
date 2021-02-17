99ef344241a7934c663628037910bb102dbcb01a
SiamSarker <sarkersiam2@gmail.com>
2021-02-17 22:00:50 +0600

    emails

---
+++ Udemy/Scripting/Emails/email_sender.py
@@ -0,0 +1,16 @@
+import smtplib
+from email.message import EmailMessage
+
+email = EmailMessage()
+email['from'] = 'Siam Sarker'
+email['to'] = 'msarker192014@bscse.uiu.ac.bd'
+email['subject'] = 'What\'s up'
+
+email.set_content('I am learning Python. ')
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login('sarkersiam2@gmail.com', '**********')
+    smtp.send_message(email)
+    print('All good!')
