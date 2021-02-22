3196f51fca3d613c2bbffcfd836f0f77a5503866
SiamSarker <sarkersiam2@gmail.com>
2021-02-22 20:14:40 +0600

    web portfolio

--- Udemy/Web Development/web server/server.py
+++ Udemy/Web Development/web server/server.py
@@ -27,15 +27,18 @@ def write_to_csv(data):
         email = data['email']
         subject = data['subject']
         message = data['message']
-        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting= csv.QUOTE_MINIMAL)
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow([email, subject, message])
 
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
-        data = request.form.to_dict()
-        write_to_csv(data)
-        write_to_file(data)
-        return redirect('/thankyou.html')
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            write_to_file(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'did not save to database'
     return 'Something went wrong. Try again!'
