817e011db6e07925c2836d6a714399485a3be7ab
SiamSarker <sarkersiam2@gmail.com>
2021-08-02 23:52:16 +0600

    knn continues

--- 212 - AI LAB/KNN/classification.py
+++ 212 - AI LAB/KNN/classification.py
@@ -1,5 +1,5 @@
 import random
-
+import math
 import numpy as np
 
 data_path = "/Users/siamsarker/Google Drive/Python/212 - AI LAB/KNN/Dataset/iris.csv"
@@ -26,7 +26,47 @@ for d in data:
 # k - Hyperparameter turing
 K = 5
 
+def euclideanDistance(val1, val2, length):
+    dist = 0
+    for i in range(length):
+        dist = dist + pow((val1[i] - val2[i]), 2)
+    return math.sqrt(dist)
+
 for V in Val_set:
+    distances = []
     for T in Train_set:
+        dist = euclideanDistance(V, T, 3)    # suppose to be N-1
+        distances.append((T, dist))
+    # for i in range(5):
+    #     print(distances[i])
+    # print("")
+
+    distances.sort(key=lambda elem: elem[1])
+
+    # for i in range(5):
+    #     print(distances[i])
+    # print("")
+    # print("")
+    near = []
+    K = 5
+    for i in range(K):
+        near.append(distances[i][0])
+
+    counts = {}
+    for i in range(len(near)):
+        IClass = near[i][-1]
+        if IClass in counts:
+            counts[IClass] = counts[IClass] + 1
+        else:
+            counts[IClass] = 1
+
+
+
+
+
+
+
+
+
 
 
