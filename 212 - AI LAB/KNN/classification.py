b7d76944a4876fdb7c98bf20ab58b188cdc9a9ca
SiamSarker <sarkersiam2@gmail.com>
2021-08-02 16:04:48 +0600

    knn

---
+++ 212 - AI LAB/KNN/classification.py
@@ -0,0 +1,25 @@
+import random
+
+import numpy as np
+
+data_path = "/Users/siamsarker/Google Drive/Python/212 - AI LAB/KNN/Dataset/iris.csv"
+my_data = np.genfromtxt(data_path, delimiter=',')
+
+data = my_data.tolist()
+
+Train_set = []
+Val_set = []
+Test_set = []
+random.shuffle(data)
+
+type(data)
+
+for d in data:
+    r = random.uniform(0, 1)
+    if 0 <= r <= 0.7:
+        Train_set.append(d)
+    elif 0.7 < r <= 0.85:
+        Val_set.append(d)
+    else:
+        Test_set.append(d)
+
