fb381582537f8e963c75b4e744519842849a2375
SiamSarker <sarkersiam2@gmail.com>
2022-02-25 19:36:45 +0600

    python

---
+++ DjangoCourse/mysite/mysite/PythonWorkshop/os_day3.py
@@ -0,0 +1,9 @@
+import os
+
+# os.makedirs("test/sub_test/sub_sub_test") #make a directory
+
+# for file in os.listdir("/Volumes/GoogleDrive/My Drive/Python/DjangoCourse/mysite/mysite/PythonWorkshop"):
+#     if file.endswith(".py"):
+#         print(file)
+
+# print(os.listdir(os.getcwd()))
